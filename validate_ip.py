c74d3e20a0f1ce51748c7a9dba1a9f0b2b1af277
Hassen Chtara <73670109+Hassen-Chtara@users.noreply.github.com>
2021-11-12 22:20:45 +0100

    Update validate_ip.py

--- validate_ip.py
+++ validate_ip.py
@@ -2,11 +2,5 @@ import re
 
 def valid_ip(address):
 
-	IPV4 = re.fullmatch('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', address)
-
-	if IPV4:
-	    return True
-
-	else:
-	    return False
+	return re.fullmatch('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', address)
 
